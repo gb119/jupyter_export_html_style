e7172559dd49e93e25fd5cd747604e9fc42461d0
Gavin Burnell <g.burnell@leeds.ac.uk>
2026-01-13 21:31:21 +0000

    Bump version to 0.0.5

--- jupyter_export_html_style/__init__.py
+++ jupyter_export_html_style/__init__.py
@@ -12,7 +12,7 @@ This package provides:
 - Integration with JupyterLab for enhanced HTML export
 """
 
-__version__ = "0.0.3"
+__version__ = "0.0.5"
 
 from .exporter import StyledHTMLExporter
 from .preprocessor import StylePreprocessor
